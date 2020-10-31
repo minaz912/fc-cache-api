7483a6acf91878a3f1412a8549a8351d04a45840
Mina S. Aziz <minaz922@gmail.com>
2020-10-31 16:32:11 +0200

    Add initial express server config and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Running');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+export const app = express();
+
+const port: number = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Express server listening on http://127.0.0.1:${port}`);
+});
