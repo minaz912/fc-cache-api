faa5d662b2bf700da65a1cfa9f15816a0a8fdddd
Mina S. Aziz <minaz922@gmail.com>
2020-10-31 16:36:27 +0200

    Tweak eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,23 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
